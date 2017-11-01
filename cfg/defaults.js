85f61b28a865ef24ac92cadcba5593ae4a999a2f
zy <ying.zheng@hand-china.com>
2017-11-01 23:23:45 +0800

    页面结构搭建

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -30,7 +30,8 @@ function getDefaultModules() {
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "firefox 15"]}'
+        // loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}'
       },
       {
         test: /\.less/,
--- src/components/Main.js
+++ src/components/Main.js
@@ -18,7 +18,7 @@ let imageDatas = require('../data/imageDatas.json');
 imageDatas = (function getImageURL(imageDataArr){
 
 	imageDataArr.forEach(function(item){
-		item.imageUrl = require("../images/"+itesm.fileName);
+		item.imageUrl = require("../images/"+item.fileName);
 	});
 
 	return imageDataArr;
@@ -29,13 +29,35 @@ imageDatas = (function getImageURL(imageDataArr){
 
 
 
+class ImgFifgure extends React.Component{
+	render(){
+		return(
+			<figure className="img-figure">
+				<img className="img-title" src={this.props.data.imageUrl} alt={this.props.data.title}/>
+				<figcaption>
+					<h2>{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		);
+	}
+}
+
 class AppComponent extends React.Component {
   render() {
+	var imgFifgures = [], 
+		controllerUtils = [];
+
+	imageDatas.forEach(function(item){
+		imgFifgures.push(<ImgFifgure data={item}/>);
+	});
+
     return (
     	<section className="stage">
     		<section className="img-sec">
+    			{imgFifgures}
     		</section>
     		<nav className="controller-nav">
+    			{controllerUtils}
 			</nav>
     	</section>
     );
